02b599e27db9a1fab6b25ba18b4a0637fed4020e
kevin_kim <kevkim1120@gmail.com>
2018-05-07 17:30:01 -0700

    testing on heroku. Changing env configs now

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+// const PORT = process.env.PORT || 5000;
+let port = process.env.PORT || 8888;
+
+const MongoClient = require('mongodb').MongoClient;
+const mongoURI = process.env.MONGOURI;
+
 const jwt = require('jsonwebtoken');
 const AWS = require('aws-sdk');
 
@@ -24,7 +30,8 @@ const region = process.env["AWS_REGION"];
 // let credentials = {clientId : clientId, clientSecret : clientSecret, refreshToken: refreshToken};
 let spotifyApi = new SpotifyWebApi({clientId : clientId,
                                     clientSecret : clientSecret,
-                                    refreshToken: refreshToken});
+                                    refreshToken: refreshToken,
+                                    redirectUri: redirectUri});
 
 // Priority serve any static files.
 app.use(cookieParser())
@@ -36,6 +43,25 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+});
+
+let db;
+MongoClient.connect(mongoURI, function(err, client) {
+  if(err)return console.log(err);
+  db = client.db('user_test');
+  console.log(`Listening on port ${port}.`);
+  app.listen(port, function () {
+    console.error(`Node cluster worker: listening on port ${port}`);
+  });
+});
+
+// Login using Spotify
+app.get('/login', function(req, res) { // Spotify Request
+  const state = generateRandomString(16);
+  const scopes = ['user-read-private', 'user-read-email', 'user-read-recently-played', 'user-top-read'];
+  res.cookie(STATE_KEY, state);
+  console.log('AUTHORIZE URL= ',spotifyApi.createAuthorizeURL(scopes, state));
+  res.redirect(spotifyApi.createAuthorizeURL(scopes, state));
 });
 
 app.get('/api/spotify', function(req, res) {
@@ -56,9 +82,3 @@ app.get('/api/spotify', function(req, res) {
     res.json([{accessToken}]);
   });
 });
-
-
-
-let port = process.env.PORT || 8888;
-console.log(`Listening on port ${port}.`);
-app.listen(port);
